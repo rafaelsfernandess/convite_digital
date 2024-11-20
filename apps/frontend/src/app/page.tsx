617ee759ce51af2c3b0c765d0cc49bc3a433fa76
Rafael <rafaelsfernandes@outlook.com>
2024-11-20 17:17:06 -0300

    Fazer lógica do evento

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Pagina from "@/components/template/Pagina";
+import { Evento } from "@/core";
 
 export default function Home() {
+  const e: Partial<Evento> = {}
   return (
     <Pagina>
       <div>Inicio</div>
